c0463af4f10df0ffe5c2c704dd2bd50f2d18d27a
Graham Martin <graham@pixely.co.uk>
2016-11-12 16:11:51 +0000

    Adding course management

---
+++ src/components/courses/courseList.js
@@ -0,0 +1,49 @@
+"use strict";
+
+var React = require('react');
+var Router = require('react-router');
+var Link = Router.Link;
+var CourseActions = require('../../actions/courseActions');
+var toastr = require('toastr');
+
+var CourseList = React.createClass({
+  propTypes: {
+      courses: React.PropTypes.array.isRequired
+  },
+  deleteCourse: function(id, event) {
+    event.preventDefault();
+    CourseActions.deleteCourse(id);
+    toastr.success('Course Deleted');
+  },
+  render: function () {
+    var createCourseRow = function (course) {
+      return (
+          <tr key={course.id}>
+            <td><a href={course.watchHref} target="_blank">Watch</a></td>
+            <td><a href="#" onClick={this.deleteCourse.bind(this, course.id)}>Delete</a></td>
+            <td><Link to="manageCourse" params={{id: course.id}}>{course.title}</Link></td>
+            <td>{course.author.name}</td>
+            <td>{course.category}</td>
+            <td>{course.length}</td>
+          </tr>
+      );
+    };
+    return (
+        <table className="table">
+          <thead>
+          <th></th>
+          <th></th>
+          <th>Title</th>
+          <th>Author</th>
+          <th>Category</th>
+          <th>Length</th>
+          </thead>
+          <tbody>
+          {this.props.courses.map(createCourseRow, this)}
+          </tbody>
+        </table>
+    );
+  }
+});
+
+module.exports = CourseList;
